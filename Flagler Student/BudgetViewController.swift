63df83067601c5969bc58d0433fd8102e11b0571
flyingking888 <jeremyfeiwang@outlook.com>
2024-02-13 12:47:50 -0500

    Added some business logic

--- Flagler Student/BudgetViewController.swift
+++ Flagler Student/BudgetViewController.swift
@@ -19,6 +19,11 @@ class BudgetViewController: UIViewController {
         
     @IBOutlet weak var scholarshipTextField: UITextField!
     
+    @IBOutlet weak var gradeLevelTextField: UITextField!
+    
+    @IBOutlet weak var honorSwitch: UISwitch!
+    
+    
     @IBOutlet weak var balanceLabel: UILabel!
     
     
@@ -26,12 +31,17 @@ class BudgetViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //This can be used to initialize values or DB lookups
+        honorSwitch.isOn = false
+        
     }
     
     //Put the IBActions right after the viewDidLoad
     
     @IBAction func calculateButton(_ sender: UIButton) {
         
+        /*
         //Get input from storyboard
         let name = nameTextField.text! //unwrapping by using the ! symbol
         let tuition = Double(tuitionTextField.text!)
@@ -42,6 +52,35 @@ class BudgetViewController: UIViewController {
         
         //Output - it's best to use interpolation in Swift!
         balanceLabel.text = "Welcome, \(name)! Your balance is: \(balance.formatted(.currency(code:"USD")))"
+        */
+        
+        if let name = nameTextField.text, !name.isEmpty, let gradeLevel = gradeLevelTextField.text, !gradeLevel.isEmpty{
+            //Attempt to covert tuition and scholarship into double type, using optional binding)
+            if let tuition = Double(tuitionTextField.text ?? ""), let scholarship = Double(scholarshipTextField.text ?? ""){
+                //main logic here
+                
+                var balance = tuition - scholarship
+                
+                //implement the honor studnet discount
+                if honorSwitch.isOn {
+                    balance = balance * (1 - 0.1)
+                }
+                
+                //make recommendation based the balance
+                if balance > 10000 {
+                    balanceLabel.text = "\(name), your balance is \(balance.formatted(.currency(code: "USD"))). Please consider a student loan."
+                }else{
+                    balanceLabel.text = "\(name), your balance is \(balance.formatted(.currency(code: "USD")))."
+                }              
+               
+                
+            }else{
+                balanceLabel.text = "Please enter valid numbers for tuition and scholarship!"
+            }
+            
+        }else{
+            balanceLabel.text = "Please enter valid name and grade!"
+        }
         
     }
     
