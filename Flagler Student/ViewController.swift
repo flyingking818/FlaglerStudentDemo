cd158764bb28a3c83979c3daa91dcee274d328a8
flyingking888 <jeremyfeiwang@outlook.com>
2024-02-20 19:15:04 -0500

    map

--- Flagler Student/ViewController.swift
+++ Flagler Student/ViewController.swift
@@ -6,21 +6,40 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var addressTextField: UITextField!
     
     @IBOutlet weak var welcomeMsg: UILabel!
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func submitButton(_ sender: UIButton) {
+    
+    @IBAction func mapSearchButton(_ sender: UIButton) {
         
-        welcomeMsg.text = "hello world!"
+        let geocoder = CLGeocoder() //object instantiation - the black box for translation address into geo coordinates.
+        if let address = addressTextField.text {
+            geocoder.geocodeAddressString(address) { [weak self] (placemarks, error) in
+                if let placemark = placemarks?.first, let location = placemark.location {
+                    let coordinate = location.coordinate
+                    self?.mapView.setRegion(MKCoordinateRegion(center: coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000), animated: true)
+                }
+            }
+        }
+        
+        
+        
     }
+    
+    
     
 }
 
